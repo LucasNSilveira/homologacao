d61bc3b8f2a98c5a4514dce0a0b915db4acfaf4a
lucas <lucasnavesdasilveira@gmail.com>
2024-08-28 15:39:23 -0300

    homologado

---
+++ app.js
@@ -0,0 +1,21 @@
+var url = 'https://newsapi.org/v2/everything?'+
+        'q=keyword&apiKey=7780dbf359384b2ab9ebd50ca6bec2bd';
+
+async function fetchData(){
+    try {
+        let response = await fetch(url);
+        let data = await response.json();
+        return data.articles;
+    }
+    catch (error){
+        console.log('Error', error);
+    }
+}
+fetchData().then(articles => {
+    articles.forEach(article => {
+        console.log("Titl: ", article.title);
+        console.log("Author: ", article.author);
+        console.log("Description: ", article.description);
+        console.log("--------------------------------");
+    });
+});
